dede75d7c55bd7862f67e187e52133000b86db9c
Matheus Pereira Garbossa <matheusgarbossa@gmail.com>
2020-10-07 17:34:28 -0300

    Removendo token de testes antigo

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ require('dotenv')
 params = {
   env: process.env.NODE_ENV,
   password: process.env.PASSWORD,
-  TOKEN: process.env.TOKEN,
   TOKEN_DISCORD: process.env.TOKEN_DISCORD
 }
 module.exports = params
